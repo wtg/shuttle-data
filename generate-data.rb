cd0a02f76d84f8e6e8b516c6c3fd388575c51c7f
Kenley Cheung <ken@lordkenthegreat.com>
2013-01-13 17:46:34 -0500

    Added stops support for generator.

--- generate-data.rb
+++ generate-data.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# generate_shapes.rb: Generate GTFS shapes.txt from current snapshot of data from shuttles.rpi.edu
+# generate-data.rb: Generate GTFS shapes.txt and stops.txt from current snapshot of data from shuttles.rpi.edu
 # Author: Kenley Cheung <cheunk3 [at] rpi [dot] edu>
 # Copyright (C) 2013 Rensselaer Polytechnic Institute
 require 'json'
@@ -12,12 +12,15 @@ data_url = "http://shuttles.rpi.edu/displays/netlink.js"
 data_json = Net::HTTP.get(URI.parse('http://shuttles.rpi.edu/displays/netlink.js'))
 netlink = JSON.parse(data_json)
 
-# Open file to write
+# Open shapes.txt for writing
 shapes = File.new("shapes.txt", "w+")
 shapes.puts('shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence');
+# Open stops.txt for writing
+stops = File.new('stops.txt', "w+")
+stops.puts('stop_id,stop_name,stop_lat,stop_lon')
 
+puts "Writing out routes.txt"
 for route in netlink["routes"]
-  # Do stuff
   counter = 0
   print "Writing out route #{route["id"]} (#{route["name"]})"
   for coordinate in route["coords"]
@@ -28,4 +31,12 @@ for route in netlink["routes"]
   print "\n"
 end
 
+puts "Writing out stops.txt"
+for stop in netlink["stops"]
+  puts "#{stop["name"]}"
+  stops.puts("#{stop["short_name"]},#{stop["name"]},#{stop["latitude"]},#{stop["longitude"]}")
+end
+
+# Close files
 shapes.close
+stops.close
